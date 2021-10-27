16624c28a1f84ed127b81bb359d5309102d20c6f
Song-EunJu <jd06280@naver.com>
2021-10-27 15:53:05 +0900

    [FIX] 등록 유저 판별 후 페이지 전환

--- Back/controllers/UserController.js
+++ Back/controllers/UserController.js
@@ -7,25 +7,49 @@ exports.saveUser = async function (req, res) {
     
     // 1. 등록된 유저인지 확인 / 토큰 교체 
     try{
-        await User.find({ email: email }, (err, users) => {
-            console.log("find");
-            console.log(users.length);
-            if(users.length!=0){ // 이미 등록되어 있으면 토큰 교체 후 해당 유저 정보를 갖고 메인으로 이동
-                console.log("update before");
-                User.updateOne({email: email}, {accessToken: accessToken});  // 토큰 교체해주는 코드 
+        const user = await User.findOne({ email: email });
 
-                return res
-                    .status(200)
-                    .json(users[0]);
-            } // 등록된 유저가 아닌 경우
-        });
+            // 이미 등록되어 있으면 토큰 교체 후 해당 유저 정보를 갖고 메인으로 이동
+        if(user){
+            console.log("update before");
+            const updateUser = await User.updateOne({email: email}, {accessToken: accessToken});
+            return res
+                .status(200)
+                .json(updateUser);  // 토큰 교체해주는 코드
+        }
     } catch (err) {
         return res
             .status(500)
             .json({ error: err });
-    }
+    } 
 
-    // 등록된 유저가 아닌경우
+    // 2. 유저가 등록되어있지 않으면
+    const user = new User({
+        email,
+        profileImage,
+        accessToken,
+        nickname
+    });
+    console.log("hi")
+    console.log(user)
+
+    try {
+        await user.save(); // 등록되어있지 않은 유저인 경우 저장하고 닉네임 페이지로 이동
+        console.log("save")
+        return res
+            .status(200)
+            .json(user) // 새로운 유저를 등록
+    } catch (err){
+        console.log("catch")
+        return res
+            .status(500)
+            .json({ error: err });
+    } 
+
+}
+
+/*
+   // 등록된 유저가 아닌경우
     const user = new User({
         email,
         profileImage,
@@ -45,5 +69,4 @@ exports.saveUser = async function (req, res) {
             .status(500)
             .json({ error: err });
     }
-}
-
+*/
